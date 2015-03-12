c9d8c3e9211a489902705bd88af621d4ac98ab9b
Julia Dart <julia.dart@digital.landregistry.gov.uk>
2015-03-12 12:05:54 +0000

    acceptance test search by title added

--- features/support/functions.rb
+++ features/support/functions.rb
@@ -17,6 +17,23 @@ def insert_title_with_owners(number_proprietors=1)
   @title
 end
 
+def insert_title_with_owners_different_title(number_proprietors=1)
+  @title = {
+    :title_number => "DN1001",
+    :street_name => "Test Street",
+    :postcode => "PL9 8TB",
+    :house_no => "14",
+    :town => "Plymouth",
+    :last_changed => "02 July 1996 at 00:59:59"
+  }
+  @title[:proprietors] = create_proprietors(number_proprietors)
+  file = File.read("./data/title_template.erb")
+  eruby = Erubis::Eruby.new(file)
+  File.write('./data/test-generated/title.json', eruby.result(binding()))
+  process_titles_in_directory("test-generated")
+  @title
+end
+
 # connect to the database and execute the sql (that deletes everything)
 def delete_all_titles
   $db_connection.exec("DELETE FROM title_register_data;")
