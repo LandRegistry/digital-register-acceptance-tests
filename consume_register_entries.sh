e1b98f83770f62c1cf66c902ae81f49b98ac530e
Camille Baldock <camille@camillebaldock.co.uk>
2015-03-13 17:19:58 +0000

    Fix acceptance test script

--- consume_register_entries.sh
+++ consume_register_entries.sh
@@ -1,3 +1,1 @@
-#TODO: this should be fixed once we know what our acceptance environments will be like
-source ~/land-registry-python-venvs/digital-register-feeder/bin/activate
-python ../consume_register_entries.py
+/usr/local/bin/python3 ../consume_register_entries.py
