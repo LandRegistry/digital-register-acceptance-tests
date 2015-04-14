57fefd11b1be0658a9630f67abd7e7d22600ff55
Julia Dart <julia.dart@digital.landregistry.gov.uk>
2015-04-14 11:45:34 +0100

    added created seperate step definitions rb file for login and user admin steps

---
+++ features/step_definitions/login_step_definitions.rb
@@ -0,0 +1,92 @@
+Given(/^I have logged in$/) do
+  @username = @new_user['user']['user_id']
+  @password = @new_user['user']['password']
+  visit("#{$DIGITAL_REGISTER_URL}/login")
+  fill_in 'username', with: @username
+  fill_in 'password', with: @password
+  click_button('signin')
+  content = page.body.text
+  expect(content).to include('Search for Title')
+end
+
+Given(/^I have a valid username and password$/) do
+  @username = @new_user['user']['user_id']
+  @password = @new_user['user']['password']
+end
+
+When(/^I log in$/) do
+  login_user(@username, @password)
+end
+
+Then(/^I should access the system$/) do
+  content = page.body.text
+  expect(content).to include('Search for Title')
+  # TODO: This is to be refactored, not happy with just checking the text on the page
+end
+
+Given(/^I have an invalid username and a valid password$/) do
+  @username = 'invalid_username'
+  @password = @new_user['user']['password']
+end
+
+Then(/^I should not access the system$/) do
+  content = page.body.text
+  expect(content).to include('There was an error with your Username/Password combination. If this problem persists please contact us at digital-register-feedback@digital.landregistry.gov.uk')
+end
+
+Given(/^I have a valid username and an incorrect password$/) do
+  @username = @new_user['user']['user_id']
+  @password = 'invalid_password'
+end
+
+Given(/^I haven't logged in$/) do
+  # Nothing happens here, the user doesnt log in with the information
+end
+
+When(/^I view the title search page$/) do
+  visit("#{$DIGITAL_REGISTER_URL}/title-search")
+end
+
+Then(/^I am redirected to the login page$/) do
+  content = page.body.text
+  expect(content).to include('Digital Register Login')
+end
+
+Given(/^I have failed to login (\d+) times$/) do |failed_login_times|
+  @username = @new_user['user']['user_id']
+  @password = 'rubbish_password'
+  failed_login_times.to_i.times do
+    login_user(@username, @password)
+  end
+end
+
+When(/^I attempt an 11th log in with the correct username and password$/) do
+  @password = @new_user['user']['password']
+  login_user(@username, @password)
+end
+
+Then(/^I am locked out of the system$/) do
+  content = page.body.text
+  expect(content).to include('There was an error with your Username/Password combination')
+end
+
+When(/^a User Admin unlocks my account$/) do
+  unlock_user_account(@username)
+end
+
+Then(/^I can login$/) do
+  @password = @new_user['user']['password']
+  login_user(@username, @password)
+  content = page.body.text
+  expect(content).to include('Search for Title')
+end
+
+When(/^I attempt a 10th correct login$/) do
+  @password = @new_user['user']['password']
+  login_user(@username, @password)
+end
+
+Then(/^it can correctly logins$/) do
+  content = page.body.text
+  expect(content).to include('Search for Title')
+end
