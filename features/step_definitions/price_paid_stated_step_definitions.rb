9044e9f7cfe883c930baa41b181078e76c61d7a0
Camille Baldock <camille@camillebaldock.co.uk>
2015-06-02 12:32:48 +0100

    Dots are now removed from PPI display

--- features/step_definitions/price_paid_stated_step_definitions.rb
+++ features/step_definitions/price_paid_stated_step_definitions.rb
@@ -15,7 +15,8 @@ Then(/^I can see the price paid or stated in the summary$/) do
   latest_price_paid_stated = @title[:price_paid_stated]
   label_index = page.all(:css, '#property-summary > dt').map(&:text).find_index('Price paid/stated')
   price_stated = page.all(:css, '#property-summary > dd')[label_index].text
-  expect(price_stated).to eq latest_price_paid_stated[:text]
+  # the final dot is now removed from PPI information
+  expect(price_stated).to eq latest_price_paid_stated[:text].chomp('.')
 end
 
 Then(/^I don't see 'not available' in the price paid or stated part of the summary$/) do
