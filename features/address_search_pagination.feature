c0671a2669f449ba7bddeabc4b8b8d8ae46a637f
Camille Baldock <camille@camillebaldock.co.uk>
2015-06-09 19:16:21 +0100

    Check that clicking next actually goes to next page

--- features/address_search_pagination.feature
+++ features/address_search_pagination.feature
@@ -13,7 +13,7 @@ Background:
     When I search for a property using only the City part of an address
     Then I can see 20 addresses per page
     And I see the number of pages is 3
-    And I can go to the next page
+    And I can go to the next page (page 2)
 
   Scenario: No address search pagination
     Given I have 10 addresses in the same City
--- features/step_definitions/pagination_step_definitions.rb
+++ features/step_definitions/pagination_step_definitions.rb
@@ -3,9 +3,11 @@ Then(/^I can see (\d+) addresses per page$/) do |address_no|
   expect(number_results).to eq address_no.to_i
 end
 
-Then(/^I can go to the next page$/) do
-  # TODO: needs verification if we actually navigated to the next page
+Then(/^I can go to the next page (page (\d+))$/) do |expected_page_num|
   click_link('Next page')
+  page_text = page.find(:css, 'div.govuk-previous-and-next-navigation > span').text
+  actual_page_num = page_text.split(' ').first.to_i
+  expect(actual_page_num).to eq expected_page_num.to_i
 end
 
 Then(/^I see we are on page (\d+)$/) do |expected_page_num|
