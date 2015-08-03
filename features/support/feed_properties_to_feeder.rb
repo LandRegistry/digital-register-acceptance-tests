a1ec20791c24711cdf95cbafd49023553f806292
Julia Dart <julia.dart@digital.landregistry.gov.uk>
2015-08-03 14:09:38 +0100

    Add invalid title def

--- features/support/feed_properties_to_feeder.rb
+++ features/support/feed_properties_to_feeder.rb
@@ -75,3 +75,13 @@ def insert_title_charity_private_individual_owner
   }
   wait_until_elasticsearch_updater_finished
 end
+
+def insert_invalid_title
+  process_titles_in_directory('insert_invalid_title')
+  @title = {
+          title_number: "AGL1013",
+          application_reference: "J991DWW",
+          last_app_timestamp: "2003-08-28T14:45:50+01:00",
+          verified: "false"
+      }
+end
