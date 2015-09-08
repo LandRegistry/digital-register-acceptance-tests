941a98f6be9d72cecbaf751baac275bf1a0a3e13
Pheonnexx <pheonn@hotmail.co.uk>
2015-09-08 16:11:37 +0100

    Add feature and inital step definitions

---
+++ features/step_definitions/official_copy_legal_txt.rb
@@ -0,0 +1,39 @@
+When(/^I attempt to view the Official Copy of the Register for a title$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then(/^I can see the edition dates in the correct format$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then(/^I can see the dates of the entries on the register in the correct format$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then(/^I can see the class of the title in the proprietorship register$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then(/^"(.*?)" is displayed on the last page of the PDF$/) do |arg1|
+  pending # express the regexp above with the code you wish you had
+end
+
+Then(/^I can see the Title number of every page$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then(/^I can see "(.*?)" is displayed on the PDF$/) do |arg1|
+  pending # express the regexp above with the code you wish you had
+end
+
+Then(/^I can see "(.*?)" is displayed on the top of the PDF$/) do |arg1|
+  pending # express the regexp above with the code you wish you had
+end
+
+Then(/^I can see that all entry dates are displayed in the UK format$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then(/^I can see that the explanation text "(.*?)" is displayed$/) do |arg1|
+  pending # express the regexp above with the code you wish you had
+end
