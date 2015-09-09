0d75e6e5085d281e1e80233d780f4ab395177f33
Pheonnexx <pheonn@hotmail.co.uk>
2015-09-09 13:45:42 +0100

    Add class of title to test data

--- features/step_definitions/official_copy_legal_txt.rb
+++ features/step_definitions/official_copy_legal_txt.rb
@@ -5,11 +5,14 @@ When(/^I attempt to view the Official Copy of the Register for a title$/) do
 end
 
 Then(/^I can see the edition dates in the correct format$/) do
-  expect(@pdf_text[0]).to include(@title[:title_number])
+  expect(@pdf_text[0]).to include('20 December 2015')
 end
 
 Then(/^I can see the dates of the entries on the register in the correct format$/) do
-  pending # express the regexp above with the code you wish you had
+  expect(@pdf_text[0]).to include('6 November 1995')
+	expect(@pdf_text[0]).to include('28 August 2014')
+	expect(@pdf_text[0]).to include('1 July 2996')
+	expect(@pdf_text[0]).to include('5 September 2005')
 end
 
 Then(/^I can see the class of the title in the proprietorship register$/) do
