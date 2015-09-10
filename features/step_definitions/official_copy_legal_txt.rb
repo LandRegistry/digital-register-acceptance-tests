b7eca9bc90f9271993c8c4951e6d383abcc2d9cd
Pheonnexx <pheonn@hotmail.co.uk>
2015-09-10 14:10:10 +0100

    Add all step definitions in ready for development

--- features/step_definitions/official_copy_legal_txt.rb
+++ features/step_definitions/official_copy_legal_txt.rb
@@ -1,5 +1,5 @@
 When(/^I attempt to view the Official Copy of the Register for a title$/) do
-	insert_title_with_owners
+	insert_title_non_private_individual_owner
 	visit_title_register_pdf(@title[:title_number])
 	convert_pdf_to_page
 end
@@ -9,36 +9,39 @@ Then(/^I can see the edition dates in the correct format$/) do
 end
 
 Then(/^I can see the dates of the entries on the register in the correct format$/) do
-  expect(@pdf_text[0]).to include('6 November 1995')
+	expect(@pdf_text[0]).to include('6 November 1995')
 	expect(@pdf_text[0]).to include('28 August 2014')
 	expect(@pdf_text[0]).to include('1 July 2996')
 	expect(@pdf_text[0]).to include('5 September 2005')
 end
 
 Then(/^I can see the class of the title in the proprietorship register$/) do
-  pending # express the regexp above with the code you wish you had
+	expect(@pdf_text[0]).to include('UNKNOWN')
 end
 
-Then(/^"(.*?)" is displayed on the last page of the PDF$/) do |arg1|
-  pending # express the regexp above with the code you wish you had
+Then(/^"(.*?)" is displayed on the last page of the PDF$/) do |legal_text|
+	expect(@pdf_text[0]).to include(legal_text)
 end
 
 Then(/^I can see the Title number of every page$/) do
-  pending # express the regexp above with the code you wish you had
+	expect(@pdf_text[0]).to include(@title[:title_number])
 end
 
-Then(/^I can see "(.*?)" is displayed on the PDF$/) do |arg1|
-  pending # express the regexp above with the code you wish you had
+Then(/^I can see "(.*?)" is displayed on the PDF$/) do |legal_text|
+	expect(@pdf_text[0]).to include(legal_text)
 end
 
-Then(/^I can see "(.*?)" is displayed on the top of the PDF$/) do |arg1|
-  pending # express the regexp above with the code you wish you had
+Then(/^I can see "(.*?)" is displayed on the top of the PDF$/) do |legal_text|
+	expect(@pdf_text[0]).to include(legal_text)
 end
 
 Then(/^I can see that all entry dates are displayed in the UK format$/) do
-  pending # express the regexp above with the code you wish you had
+	expect(@pdf_text[0]).to include('6 November 1995')
+	expect(@pdf_text[0]).to include('28 August 2014')
+	expect(@pdf_text[0]).to include('1 July 2996')
+	expect(@pdf_text[0]).to include('5 September 2005')
 end
 
-Then(/^I can see that the explanation text "(.*?)" is displayed$/) do |arg1|
-  pending # express the regexp above with the code you wish you had
+Then(/^I can see that the explanation text "(.*?)" is displayed$/) do |explanation_text|
+	expect(@pdf_text[0]).to include(explanation_text)
 end
