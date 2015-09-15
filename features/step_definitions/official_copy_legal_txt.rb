1e8a10f45df51a6a2ab6e41210f1a741403fc957
Pheonnexx <pheonn@hotmail.co.uk>
2015-09-15 13:59:29 +0100

    changes to checking text in pdf

--- features/step_definitions/official_copy_legal_txt.rb
+++ features/step_definitions/official_copy_legal_txt.rb
@@ -5,45 +5,46 @@ When(/^I attempt to view the Official Copy of the Register for a title$/) do
 end
 
 Then(/^I can see the edition dates in the correct format$/) do
-  expect(@pdf_text[0]).to include('20 December 2015')
+  expect(@pdf_pages[0]).to include('20 December 2015')
 end
 
 Then(/^I can see the dates of the entries on the register in the correct format$/) do
-	expect(@pdf_text[0]).to include('6 November 1995')
-	expect(@pdf_text[0]).to include('28 August 2014')
-	expect(@pdf_text[0]).to include('1 July 2996')
-	expect(@pdf_text[0]).to include('5 September 2005')
+	expect(@pdf_pages[0]).to include('6 November 1995')
+	expect(@pdf_pages[0]).to include('28 August 2014')
+	expect(@pdf_pages[0]).to include('1 July 2996')
+	expect(@pdf_pages[0]).to include('5 September 2005')
 end
 
 Then(/^I can see the class of the title in the proprietorship register$/) do
-	expect(@pdf_text[0]).to include('UNKNOWN')
+	expect(@pdf_pages[0]).to include('Absolute')
 end
 
 Then(/End of register is displayed on the last page of the PDF$/) do
-	expect(@pdf_text[3]).to match('End\s?of\s?register')
+	expect(@pdf_pages[3]).to match('End\s?of\s?register')
 end
 
 Then(/^I can see the Title number of every page$/) do
-	expect(@pdf_text[0]).to include("#{@title[:title_number]} Page 1")
-	expect(@pdf_text[1]).to include("#{@title[:title_number]} Page 2")
-	expect(@pdf_text[2]).to include("#{@title[:title_number]} Page 3")
+	expect(@pdf_pages[0]).to include("Title number:#{@title[:title_number]} Page 1")
+	expect(@pdf_pages[1]).to include("Title number:#{@title[:title_number]} Page 2")
+	expect(@pdf_pages[2]).to include("Title number:#{@title[:title_number]} Page 3")
 end
 
+# regex has been used in matching text in the pdf due to odd spacing when parsing
 Then(/^I can see "(.*?)" is displayed on the PDF$/) do |legal_text|
-	expect(@pdf_text[0]).to include(legal_text)
+	expect(@pdf_pages[0]).to match(legal_text.gsub(" ", "\s?"))
 end
 
 Then(/^I can see "(.*?)" is displayed on the top of the PDF$/) do |legal_text|
-	expect(@pdf_text[0]).to include(legal_text)
+	expect(@pdf_pages[0]).to match(legal_text.gsub(" ", "\s?"))
 end
 
 Then(/^I can see that all entry dates are displayed in the UK format$/) do
-	expect(@pdf_text[0]).to include('6 November 1995')
-	expect(@pdf_text[0]).to include('28 August 2014')
-	expect(@pdf_text[0]).to include('1 July 2996')
-	expect(@pdf_text[0]).to include('5 September 2005')
+	expect(@pdf_pages[0]).to include('6 November 1995')
+	expect(@pdf_pages[0]).to include('28 August 2014')
+	expect(@pdf_pages[0]).to include('1 July 2996')
+	expect(@pdf_pages[0]).to include('5 September 2005')
 end
 
 Then(/^I can see that the explanation text "(.*?)" is displayed$/) do |explanation_text|
-	expect(@pdf_text[0]).to include(explanation_text)
+	expect(@pdf_pages[0]).to match(explanation_text.gsub(" ", "\s?"))
 end
