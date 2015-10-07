4bbf80e7f4b83a0701dcb7cdb10474907480c50f
barbarahowell2 <barbara.howell@digital.landregistry.gov.uk>
2015-10-07 15:11:41 +0100

    Add feature and step definitions

---
+++ features/step_definitions/remove_delimiters_on_pdf.rb
@@ -0,0 +1,7 @@
+When(/^I attempt to view the Official Copy of the Register which contains delimiters$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then(/^no delimiters are displayed$/) do
+  pending # express the regexp above with the code you wish you had
+end
