e54b9190b3fbbb6bcd02125a120df7c7ff02d0a7
barbarahowell2 <barbara.howell@digital.landregistry.gov.uk>
2015-10-07 14:48:11 +0100

    Add feature and step definitions

---
+++ features/step_definitions/numbering_schedule_of_leases_on_pdf.rb
@@ -0,0 +1,7 @@
+When(/^I attempt to view the Official Copy of the Register that contains a schedule of leases$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then(/^I can see that the schedule entries are numbered consecutively$/) do
+  pending # express the regexp above with the code you wish you had
+end
