137326e0d41dad9113856fc518a73e1e5dada0ca
Julia Dart <julia.dart@digital.landregistry.gov.uk>
2015-10-16 14:58:23 +0100

    add feature file and step definition snippets

---
+++ features/step_definitions/login_existing_FAP_user.rb
@@ -0,0 +1,23 @@
+Given(/^I am an existing Find a property user$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+When(/^I attempt to Log in$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then(/^I am able to access the service$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Given(/^my account has expired$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then(/^I am unable to access the service$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then(/^an explanatory message is displayed$/) do
+  pending # express the regexp above with the code you wish you had
+end
