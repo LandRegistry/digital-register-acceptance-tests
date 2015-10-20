43a42833a2109ffd92ca8dba1177c6cc325b48a4
Pheonnexx <pheonn@hotmail.co.uk>
2015-10-20 17:45:31 +0100

    Fixed story us135 and us69

--- features/step_definitions/filter_closed_titles_step_definitions.rb
+++ features/step_definitions/filter_closed_titles_step_definitions.rb
@@ -7,13 +7,11 @@ Given(/^I have a closed title$/) do
   closure_status = 'TITLE CLOSED'
   # In this case, do not wait for updater: the title is not inserted
   # because it is closed
-  closed_title = insert_title_with_owners(1, closure_status, false)
+  insert_title_with_owners(1, closure_status, false)
   # insert open title with an elasticsearch wait
   # this enables us to check that the elasticsearch updater has had
   # time to run but the title is not visible
-  insert_title_with_owners
   # reset @title to the closed title for the rest of the test
-  @title = closed_title
 end
 
 Then(/^an unavailable message is displayed$/) do
@@ -39,7 +37,7 @@ end
 Then(/^the title summary page is displayed$/) do
   expect(content).to include(@title[:postcode])
   expect(content).to include(@title[:town])
-  expect(content).to include("#{@title[:house_no]} #{@title[:street_name]}")
+  expect(content).to include("#{@title[:house_no]}#{@title[:house_alpha]} #{@title[:street_name]}")
   expect(content).to include(@title[:title_number])
   expect(content).to include(@title[:proprietors][0][:name])
 end
