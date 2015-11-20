c201d3d755566a704cc4a6821a1bd5614fe669cf
juliadart <julia.dart@digital.landregistry.gov.uk>
2015-11-20 09:27:55 +0000

    add step definition snippets

---
+++ features/step_definitions/welsh-language-support.rb
@@ -0,0 +1,15 @@
+When(/^I have selected the Welsh translation$/) do
+click_link('in Welsh (Cymraeg)')
+end
+
+Then(/^I can view the summary information in Welsh$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+When(/^I select the English translation$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then(/^I can view the summary information in English$/) do
+  pending # express the regexp above with the code you wish you had
+end
