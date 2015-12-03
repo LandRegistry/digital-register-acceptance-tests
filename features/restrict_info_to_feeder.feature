e70aa09b6fa8f5f522c0bd45e96b57b95d79bce6
juliadart <julia.dart@digital.landregistry.gov.uk>
2015-12-03 16:09:18 +0000

    add step definition snippets

--- features/restrict_info_to_feeder.feature
+++ features/restrict_info_to_feeder.feature
@@ -13,12 +13,12 @@ The flow: -
 
 
 Scenario: Unable to search a non Plymouth or Coventry title number
-  Given I have an title which is not in a Plymouth or Coventry district
+  Given I have a title which is not in a Plymouth or Coventry district
   When I search for the title
   Then I am unable to view it
 
 Scenario: Unable to select non Plymouth or Coventry results
   Given I have an address which is not in a Plymouth or Coventry district
   When I search for the address
-  Then I am displayed a list of results
+  Then I will be displayed a list of results
   And I am unable to select a result
--- features/step_definitions/restrict_info_to_feeder.rb
+++ features/step_definitions/restrict_info_to_feeder.rb
@@ -1,8 +1,4 @@
-Given(/^I have an title which is not in a Plymouth or Coventry district$/) do
-  pending # express the regexp above with the code you wish you had
-end
-
-Then(/^no results are found$/) do
+Given(/^I have a title which is not in a Plymouth or Coventry district$/) do
   pending # express the regexp above with the code you wish you had
 end
 
@@ -11,10 +7,6 @@ Given(/^I have an address which is not in a Plymouth or Coventry district$/) do
 end
 
 When(/^I search for the address$/) do
-  pending # express the regexp above with the code you wish you had
-end
-
-Then(/^I am displayed a list of results$/) do
   pending # express the regexp above with the code you wish you had
 end
 
