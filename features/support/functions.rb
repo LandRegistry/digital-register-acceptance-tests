9de712e06207cac5fc1b4e59b5afab99c790a10e
Heather Poole <pheonn@hotmail.co.uk>
2015-12-08 16:42:39 +0000

    Revert "Amend find function in login script"

--- features/support/functions.rb
+++ features/support/functions.rb
@@ -8,7 +8,7 @@ include W3CValidators
 
 def login_if_webseal_present
   if $TEST_ENVIRONMENT_WEBSEAL == 'integration' then
-    if content.has_text?('You are already logged in') then
+    if content.find('You are already logged in') then
       click('Terminate existing login')
     end
     fill_in 'username', with: $WEBSEAL_LOGIN
