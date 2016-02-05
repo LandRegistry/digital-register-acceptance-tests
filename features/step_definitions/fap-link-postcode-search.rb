3fc6473d389ce576adcf8c867c88923cb4c9a19d
juliadart <julia.dart@digital.landregistry.gov.uk>
2016-02-05 10:57:31 +0000

    Add feature and step definitions

---
+++ features/step_definitions/fap-link-postcode-search.rb
@@ -0,0 +1,16 @@
+Given(/^I am on the title search page$/) do
+  visit_title_search_page
+end
+
+Given(/^I don’t know the postcode$/) do
+  find("summary", :text => "Don't know the postcode?").click
+end
+
+When(/^I click the by street address link$/) do
+  link = find_link('by title number')[:href]
+  expect(link).to eq('https://eservices.landregistry.gov.uk/wps/portal/Property_Search')
+end
+
+Then(/^I can access FaP$/) do
+#no code here as covered in previous step
+end
