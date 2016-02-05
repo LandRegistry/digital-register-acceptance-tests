c0346ffa22baf75ef12b4b0d67aa474179f0b95d
Andy Mantell <andymantell@hotmail.com>
2016-02-05 14:53:17 +0000

    Commit feature file and step defintitons for the caution title explanation on the search results page

---
+++ features/step_definitions/caution_title_search_results.rb
@@ -0,0 +1,12 @@
+When(/^I click on the Whats this link$/) do
+  find("summary", :text => "What's this?").click
+end
+
+Then(/^I see an explanation of what caution title means$/) do
+  expect(content).to include('A caution against first registration protects an interest')
+end
+
+Then(/^the link in the explanation goes to the gov\.uk explanation page$/) do
+  link = find_link('More about cautions against first registration')[:href]
+  expect(link).to eq('https://www.gov.uk/government/publications/caution-against-first-registration/practice-guide-3-cautions-against-first-registration')
+end
