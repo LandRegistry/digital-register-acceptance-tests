22fb14fb254b7b52749e298e5358c8abf3fcb34a
juliadart <julia.dart@digital.landregistry.gov.uk>
2016-02-05 10:56:18 +0000

    Add feature and step definitions

---
+++ features/step_definitions/no_title_info_message.rb
@@ -0,0 +1,15 @@
+Given(/^I have a property with no title information$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+When(/^I click on the ‘why not’ link$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then(/^I am given an explanation of why this may have occurred$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then(/^a link to the FaP search page is displayed$/) do
+  pending # express the regexp above with the code you wish you had
+end
