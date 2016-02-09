530303af574034edfbac2423f004c134339b798c
woodingd <dmrwooding@gmail.com>
2016-02-09 11:19:06 +0000

    Changed test to look for new sidebar id

--- features/step_definitions/help_on_search_results.rb
+++ features/step_definitions/help_on_search_results.rb
@@ -5,15 +5,15 @@ Given(/^I am viewing the search results page$/) do
 end
 
 Then(/^I will be displayed a help message in the sidebar$/) do
-  expect(find('#content .column-third')).to have_content('Help finding the right property')
+  expect(find('#help-sidebar')).to have_content('Help finding the right property')
 end
 
 Then(/^I will be able to click a link to get in touch$/) do
-  link = find('#content .column-third a', :text => 'let us know')[:href]
+  link = find('#help-sidebar a', :text => 'let us know')[:href]
   expect(link).to eq('mailto:digital-register-feedback@digital.landregistry.gov.uk')
 end
 
 Then(/^I will be able to click a link to go to FaP$/) do
-  link = find('#content .column-third a', :text => 'existing Land Registry service')[:href]
+  link = find('#help-sidebar a', :text => 'existing Land Registry service')[:href]
   expect(link).to eq('https://eservices.landregistry.gov.uk/wps/portal/Property_Search')
 end
