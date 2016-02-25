f8b7490846eb4095f62f2e46000baf6c46859106
Pheonnexx <pheonn@hotmail.co.uk>
2016-02-25 11:31:46 +0000

    further acceptance criteria

--- features/step_definitions/service_information.rb
+++ features/step_definitions/service_information.rb
@@ -2,10 +2,34 @@ Given(/^I have gone to the digital register view service$/) do
   pending # express the regexp above with the code you wish you had
 end
 
-Then(/^I can confirm the price of the service will be £(\d+)\.(\d+)$/) do |arg1, arg2|
+Then(/^I can confirm the price of the service will be £(\s+)$/) do |arg1|
   pending # express the regexp above with the code you wish you had
 end
 
 Then(/^I am provided information on what the service will provide$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+When(/^I select to use the new service$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then(/^I am redirected to digital register view$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+When(/^I select that I need full title documents$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then(/^I am redirected to the find a property service$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+When(/^I select that I need Official Copies$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then(/^I am redirected to the Land Registry information on ordering official copies$/) do
   pending # express the regexp above with the code you wish you had
 end
