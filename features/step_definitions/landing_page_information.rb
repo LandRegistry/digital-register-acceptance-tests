8db1a7990a9701baaa2257ef6224fe5b911c80da
juliadart <julia.dart@digital.landregistry.gov.uk>
2016-03-09 17:06:59 +0000

    added code to step definitions and renamede feature file

--- features/step_definitions/landing_page_information.rb
+++ features/step_definitions/landing_page_information.rb
@@ -1,29 +1,32 @@
 Given(/^I have gone to the digital register view service$/) do
-  pending # express the regexp above with the code you wish you had
+  visit_landing_page
 end
 
-Then(/^I can confirm the price of the service will be £(\s+)$/) do |arg1|
-  pending # express the regexp above with the code you wish you had
+Then(/^I can confirm the price of the service will be £(\d+)\.(\d+)$/) do |arg1, arg2|
+  expect(content).to include('£3 inc VAT')
 end
 
 Then(/^I am provided information on what the service will provide$/) do
-  pending # express the regexp above with the code you wish you had
+  expect(content).to include('Available from this service',"Available from Land Registry ‘Find a Property’",'Available by post')
 end
 
 When(/^I select to use the new service$/) do
-  pending # express the regexp above with the code you wish you had
-end
+  choose("eligibility-1")
+  find_button('Continue').click
+ end
 
 Then(/^I am redirected to digital register view$/) do
-  pending # express the regexp above with the code you wish you had
+  visit_property_search_page
 end
 
 When(/^I select that I need full title documents$/) do
-  pending # express the regexp above with the code you wish you had
+  choose("eligibility-2")
+  find_button('Continue').click
 end
 
 Then(/^I am redirected to the find a property service$/) do
-  pending # express the regexp above with the code you wish you had
+  #expect(link).to eq('https://eservices.landregistry.gov.uk/wps/portal/Property_Search')
+  expect(page).to have_selector(:xpath, "https://eservices.landregistry.gov.uk/wps/portal/Property_Search")
 end
 
 When(/^I select that I need Official Copies$/) do
