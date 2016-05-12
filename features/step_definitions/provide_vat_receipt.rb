9ab2323c206f0646d178c7975e5ef6a00e533947
Andy Mantell <andymantell@hotmail.com>
2016-05-12 16:12:10 +0100

    Write failing test for the welsh VAT receipt (after clicking the translate link)

--- features/step_definitions/provide_vat_receipt.rb
+++ features/step_definitions/provide_vat_receipt.rb
@@ -4,7 +4,7 @@ When(/^I choose to buy a title Summary$/) do
 end
 
 Then(/^I can view the title VAT receipt on the Summary$/) do
-   expect(content).to include('VAT receipt')
+  expect(content).to include('VAT receipt')
 end
 
 Then(/^it has the correct information$/) do
@@ -14,7 +14,21 @@ Then(/^it has the correct information$/) do
   expect(content).to include("VAT @ 20%")
   expect(content).to include("GB 8888 181 53")
   expect(content).to include("Land Registry Trafalgar House 1 Bedford Park Croydon CR0 2AQ")
+end
 
+Then(/^I can view the title VAT receipt on the Summary in Welsh$/) do
+  # Test temporarily written in English until we get the translations through
+  expect(content).to include('VAT receipt')
+end
+
+Then(/^it has the correct information in welsh$/) do
+  # Test temporarily written in English until we get the translations through
+  expect(content).to include("Date")
+  expect(content).to include(Date.today().strftime('%d %B %Y').to_s)
+  expect(content).to include("Transaction ID")
+  expect(content).to include("VAT @ 20%")
+  expect(content).to include("GB 8888 181 53")
+  expect(content).to include("Land Registry Trafalgar House 1 Bedford Park Croydon CR0 2AQ")
 end
 
 Then(/^I can print the title summary page$/) do
@@ -24,3 +38,4 @@ end
 Then(/^the VAT receipt is on the printed result$/) do
 #Unable to automate this, it will have to be checked manually
 end
+
